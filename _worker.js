adff087627485047bd17ce8d919abeb05bd4acd3
Jason <jason-git@outlook.com>
2023-11-10 03:43:50 +0800

    test

--- _worker.js
+++ _worker.js
@@ -1,29 +1,36 @@
+// _worker.js
 
 const TELEGRAPH_URL = 'https://api.nextweb.fun';
 
-
-export default {
-  async fetch(request, env) {
-      const NewResponse = await handleRequest(request)
-      return NewResponse
-  },
-
-};
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request) {
+  // 检查请求的路径
   const url = new URL(request.url);
-  const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
+  if (url.pathname === '/') {
+    // 如果请求的是根路径，返回 index.html 文件内容
+    const indexHtmlResponse = await fetch('https://nextai.date');
+    return indexHtmlResponse;
+  }
+
+  // 如果请求的不是根路径，将其代理到指定的 URL
   url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
+
   const modifiedRequest = new Request(url.toString(), {
     headers: request.headers,
     method: request.method,
     body: request.body,
     redirect: 'follow'
   });
+
   const response = await fetch(modifiedRequest);
   const modifiedResponse = new Response(response.body, response);
-  // 添加允许跨域访问的响应头
-  modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
+
+  // 添加允许跨域访问的响应头，从请求中获取 Origin
+  const origin = request.headers.get('Origin') || '*';
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', origin);
+
   return modifiedResponse;
 }
-
